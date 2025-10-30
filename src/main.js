d942bff29fa80565224f0e9e4d9bf059e6a7f78e
Andrei Sergheev <chaos95kill@gmail.com>
2025-10-30 16:24:01 +0200

    First settings

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './style.css';
+// import logo from './icons/logo.svg';
+// import menu from './icons/menu.svg';
+// import close from './icons/close.svg';
+// import favorite from './icons/favorite.svg';
+// import search from './icons/search.svg';
+// import shoppingCart from './icons/shopping-bag.svg';
+// import star from './icons/star.svg';
+// import Icons from './icons/' as *;
+import 'virtual:svg-icons-register';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const iconContainer = document.getElementById('icon-container');
+  // const iconName = 'home'; // Имя файла home.svg
+  const iconMarkup = `<!--<svg class="icon"><use href="#icon-${iconName}" /></svg>-->`;
+  // iconContainer.innerHTML = iconMarkup;
+});
