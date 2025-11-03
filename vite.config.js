79381410812456f9c97cb34b081570194063e10b
Andrei Sergheev <chaos95kill@gmail.com>
2025-11-03 16:40:50 +0200

    Add favicon

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 
 export default defineConfig ( {
+  base: './', // делаем пути в dist относительными
   resolve: {
     alias: {
       "@": path.resolve ( __dirname, "./src" ),
